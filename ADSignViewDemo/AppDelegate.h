30d8e7fe9bb3ce16292fe9f93ec44217f45ff872
安德航 <bjwltiankong@163.com>
2016-10-28 18:16:13 +0800

    Initial Commit

---
+++ ADSignViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADSignViewDemo
+//
+//  Created by andehang on 2016/10/28.
+//  Copyright © 2016年 andehang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADSignViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADSignViewDemo
+//
+//  Created by andehang on 2016/10/28.
+//  Copyright © 2016年 andehang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
