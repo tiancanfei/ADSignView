33ed71edbf2e29c248d96a80d2a447d877353a74
安德航 <bjwltiankong@163.com>
2017-02-09 11:43:22 +0800

    同步

--- ADSignViewDemo/ADSignView/ADSignView.h
+++ ADSignViewDemo/ADSignView/ADSignView.h
@@ -11,12 +11,10 @@
 @interface ADSignView : UIView
 
 - (void)erase;
-
 /**
  保存签名
  image:原图
  imageData:原图的NSData文件
  */
 - (void)saveSign:(void(^)(UIImage *image,NSData *imageData))saveImageBlock;
-
 @end
